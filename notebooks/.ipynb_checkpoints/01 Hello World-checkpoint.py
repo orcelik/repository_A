8bc0cd9f6ed22e4b25a730832fd8e1c155127ea5
orcelik <42545856+orcelik@users.noreply.github.com>
2018-11-28 11:58:09 +0200

    updated

---
+++ notebooks/.ipynb_checkpoints/01 Hello World-checkpoint.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %md # "Hello world" in Python!
+
+# COMMAND ----------
+
+# MAGIC %md When programmers are learning a new language, we tend to write a one-line program that prints some version of the message "Hello world!" this is a simple program that shows whether your computer is properly set up to run Python programs.
+
+# COMMAND ----------
+
+print('Hello Python world!') 
+
+# COMMAND ----------
+
+print('end of program!')
+
+# COMMAND ----------
+
+# MAGIC %md If it works, congratulations! You just ran your first Python program.
+
+# COMMAND ----------
+
